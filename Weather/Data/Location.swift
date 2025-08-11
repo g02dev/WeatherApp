cb023d1412886c5cd7e9f2ebb144688abdbfcb4e
g02dev <58611737+g02dev@users.noreply.github.com>
2025-08-11 19:41:56 +0100

    Add unique constraint for favourite locations

--- Weather/Data/Location.swift
+++ Weather/Data/Location.swift
@@ -4,10 +4,18 @@ struct Location: Hashable, Identifiable {
     let name: String
     let latitude: Double
     let longitude: Double
-    var country: String
-    var state: String?
+    let country: String
+    let state: String?
     
-    var id: Self { self }
+    var id: Int { self.hashValue }
+    
+    init(name: String, latitude: Double, longitude: Double, country: String, state: String? = nil) {
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.country = country
+        self.state = state
+    }
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(latitude)
